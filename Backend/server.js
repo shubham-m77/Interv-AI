b9167274d7bfac1de2b00d12044a463d7f0d3025
Shubham Mandal <shubham.m9022@gmail.com>
2026-04-15 13:26:10 +0530

    Edited Code

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const connectDB = require('./src/config/db');
 const authRouter = require('./src/routes/auth.routes');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const interviewRouter = require('./src/routes/interview.routes');
-const { default: mongoose } = require('mongoose');
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cookieParser());
@@ -13,34 +14,11 @@ app.use(cors({
     credentials: true 
 }));
 app.use(express.json());
+connectDB();
 
-let isConnected = false;
-
-// Connectin with database and starting the server
-const connectDB = async () => {
-  try {
-     if (isConnected) return; // reuse existing connection
-    await mongoose.connect(process.env.MONGODB_URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    isConnected = true;
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-   
-};
-
-// call before every route
-app.use(async (req, res, next) => {
-  if(!isConnected) {
-    await connectDB();
-  }
-  next();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use('/api/auth', authRouter);
 app.use('/api/interview', interviewRouter);
-
-module.exports = app;
